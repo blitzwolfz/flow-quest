475b61cbce1af83d4a081f14e18b374b3b12ab9a
Sam <qureshisamin25@gmail.com>
2023-12-22 16:45:38 -0500

    Added some css

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -6,9 +6,10 @@ interface TodoCardProps {
 
 const TodoCard: React.FC<TodoCardProps> = ({ todo }) => {
     return (
-        <div className="card">
+        <div className="card" style={{backgroundColor: "lightblue"}}>
             <h3>{todo.description}</h3>
             <p>{todo.done ? 'Completed' : 'Not Completed'}</p>
+            <p>{todo.date}</p>
         </div>
     );
 };
